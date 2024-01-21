69d5e292530dd073c7cfd050bfb3feb831cc0d7a
kartik2433 <95977507+kartik2433@users.noreply.github.com>
2024-01-21 15:46:05 +0530

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,57 @@
+import dotenv from "dotenv";
+import {app} from "./app.js"
+import ConnectDB from "./db/index.js";
+
+dotenv.config({
+    path: "../env"
+});
+
+ConnectDB()
+// .then{
+//     app.on("error",()=>{
+//         console.log(error);    
+//     })
+//     app.listen(process.env.PORT,()=>{
+//         console.log(`Server is running on port ${process.env.PORT}`);
+//     })
+// }
+// .catch(error){
+//     console.log(error);
+// }
+// app.get("/",(req,res)=>{
+//     res.send("Hello World")
+// });
+
+
+// import express from "express";
+// import mongoose from "mongoose";
+// import dotenv from "dotenv";
+// dotenv.config();
+// import {DB_NAME} from "./constants.js";
+// const app = new express();
+// app.get("/",(req,res)=>{
+//     res.send("Hello World");
+// });
+// //ifies
+// ;( async()=>{
+//     try {
+//         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)    
+        
+//         app.on("Error",(error)=>{
+//             console.log("Err: ",error);    
+//             throw error
+//         })
+
+//         app.listen(3000, () => {
+//           console.log("Server is running on port 3000");    
+//         });
+
+//         console.log(
+//           `MongoDB Connected Successfully...!!! \n DB HOST: ${connectionInstance.connection.host}`
+//         );
+
+//     } catch (error) {
+//         console.error("ERROR: ", error);    
+//         throw error
+//     }
+// })()
---
+++ src/models/user.model.js
@@ -0,0 +1,68 @@
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true, // included if we want to make username as searchable Field
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    avatar: {
+      type: String, // Cloudinary url
+      required: true,
+    },
+    coverImage: {
+      type: String, // Cloudinary url
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password")) return  next()
+
+    this.password = bcrypt.hash(this.password, 10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function (password){
+    return await bcrypt.compare(password, this.password)
+}
+
+
+
+export const User = mongoose.model("User", userSchema);
