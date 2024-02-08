2284eeffd24d422295d525a38057158bf0f69b2d
kartik2433 <95977507+kartik2433@users.noreply.github.com>
2024-02-09 00:08:01 +0530

    Added Middleware

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,21 @@ import { uploadOnCloudinary } from "../utils/cloudinary.js"
 import {ApiError} from "../utils/ApiError.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 
+const generateAccessAndRefreshTokens = async(userId)=>{
+    try{
+        const user = await User.findById(userId)
+        const accessToken = user.generateAccessToken();
+        const refreshToken = user.generateRefreshToken();
+
+        user.refreshToken = refreshToken;
+        await user.save({ validateBeforeSave : false });
+
+        return  {accessToken,refreshToken}; 
+    }catch(error){
+        throw new ApiError("An error occured while generating tokens")
+    }
+} 
+
 const registerUser = asyncHandler(async(req,res) => {
     const { fullName, email, username, password } = req.body;
     console.log("fullname: ",fullName);
@@ -21,7 +36,6 @@ const registerUser = asyncHandler(async(req,res) => {
     if (existedUser){
         throw new ApiError(400,"User already exists");
     }
-
     console.log(req.files);
     const avatarLocalPath = req?.files?.avatar[0]?.path;
   
@@ -69,7 +83,81 @@ const registerUser = asyncHandler(async(req,res) => {
     // });
 })
 
-export {registerUser}
+const loginUser = asyncHandler(async(req,res) => {
+    const {username,email,password} = req.body;
+    
+    if(!username || !email){
+        throw new ApiError(400, "Username or Email is Required..!!")
+    }
+
+    const user = await User.findOne({
+        $or:[{username},{email}]
+    })
+
+    if(!user){
+        throw new ApiError(404,'User Does Not Exist')
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password)
+
+    if(!isPasswordValid){
+        throw new ApiError(400, "Invalid Credentials");
+    }
+
+    const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(
+      user._id
+    );
+
+    const loggedInUser = await  User.findById(user._id).select("-password -refreshToken")
+
+    const options = {
+        httpOnly: true,
+        secure: true,
+    }
+
+    return res
+      .status(200)
+      .cookie("accessToken", accessToken, options)
+      .cookie("refreshToken", refreshToken, options)
+      .json(
+        new ApiResponse(200, {
+          user: loggedInUser,accessToken,refreshToken
+        },
+        "User Logged In Successfully."
+        )
+      );
+
+})
+
+const logoutUser = asyncHandler(async(req,res) => {
+    User.findByIdAndUpdate(
+      req.user._id,
+      {
+        $set: {
+          refreshToken: undefined,
+        },
+      },
+      {
+        new: true,
+      }
+    );
+
+    const options = {
+      httpOnly: true,
+      secure: true,
+    };
+
+    return res
+      .status(200)
+      .clearCookie("accessToken", options)
+      .clearCookie("refreshToken", options)
+      .json(
+        new ApiResponse(200, {}, "User logged Out")
+      ) 
+})
+
+export { registerUser, loginUser, logoutUser };
+
 /* 
     Steps of registering user:
     1. Get user details from the request body
